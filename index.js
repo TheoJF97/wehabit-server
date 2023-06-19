2ce48cdd7ab41a544237acdfb007ad7cca31f592
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-18 20:38:56 -0400

    Setup user route, and controller middleware

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const wehabitRoutes = require("./routes/wehabit");
+const userRoutes = require("./routes/user-routes");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/habit", wehabitRoutes);
+app.get("/", (_req, res) => {
+  res.send("WELCOME TO MY API");
+});
+
+// MW that GETs all users 
+app.use("/users", userRoutes);
 
 app.listen(PORT, function () {
   console.log(`Server is running on ${PORT}`);
