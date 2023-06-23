c93e3dc7f63deb21fb030f54bb1d08fa252607f7
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-23 07:00:45 -0400

    Refactor /signup /login /profile endpoints

--- index.js
+++ index.js
@@ -31,69 +31,66 @@ app.use("/encouragemints", encouragemintsRoutes);
 // MW for completions
 app.use("/completions", completionsRoutes);
 
+// SIGNUP AND LOGIN ENDPOINTS
+app.use((req, res, next) => {
+  // Signup and login are public URLs that don't require a token
+  if (req.url === "/signup" || req.url === "/login") {
+    next();
+  } else {
+    // Format of request is BEARER <token>. Splitting on ' ' will create an
+    // array where the token is at index 1
+    const token = getToken(req);
+
+    if (token) {
+      console.log("Auth Token:", token);
+      if (jwt.verify(token, process.env.SECRET_KEY)) {
+        // Decode the token to pass along to end-points that may need
+        // access to data stored in the token.
+        req.decode = jwt.decode(token);
+        next();
+      } else {
+        res.status(403).json({ error: "Not Authorized." });
+      }
+    } else {
+      res.status(403).json({ error: "No token. Unauthorized." });
+    }
+  }
+});
+
+function getToken(req) {
+  return req.headers.authorization.split(" ")[1];
+}
+
 const users = {};
 
-// signup endpoint
 app.post("/signup", (req, res) => {
   const { username, name, password } = req.body;
   users[username] = {
     name,
-    password, // NOTE: Passwords should NEVER be stored in the clear like this. Use a
-    // library like bcrypt to Hash the password. For demo purposes only.
+    password, // NOTE: Passwords should NEVER be stored in the clear like this. Use a              // library like bcrypt to Hash the password. For demo purposes only.
   };
+  console.log("Users Object:", users);
   res.json({ success: "true" });
 });
 
-// A Basic Login end point
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   const user = users[username];
-  console.log(user);
   if (user && user.password === password) {
-    // Create a JWT token for the user, and add their name to the token,
-    const token = jwt.sign(
-      { username: username }, //payload
-      process.env.SECRET_KEY //signature
-    );
-    // Send the token back to the client
-    return res.status(200).json(token);
+    console.log("Found user:", user);
+    res.json({ token: jwt.sign({ name: user.name }, process.env.SECRET_KEY) });
   } else {
-    return res.status(401).json({
-      message: "You shall not pass!",
+    res.status(403).json({
+      token: "",
+      error: {
+        message: "Error logging in. Invalid username/password combination.",
+      },
     });
   }
 });
 
-const authorize = (req, res, next) => {
-  const splitAuthorizationHeader = req.headers.authorization.split(" ");
-  console.log(splitAuthorizationHeader);
-
-  if (splitAuthorizationHeader.length !== 2) {
-    return res.status(403).json({
-      message: "endpoint requires a valid token",
-    });
-  }
-
-  const bearerToken = splitAuthorizationHeader[1];
-  console.log(bearerToken);
-  console.log(process.env.SECRET_KEY);
-
-  jwt.verify(bearerToken, process.env.SECRET_KEY, (error, decoded) => {
-    if (error) {
-      console.log(error);
-      return res.status(403).json({
-        message: "Invalid JWT",
-      });
-    }
-
-    req.iat = decoded.iat;
-    console.log(decoded);
-    next();
-  });
-};
-
-app.get("/profile", authorize, (req, res) => {
-  res.json(req.iat);
+app.get("/profile", (req, res) => {
+  res.json(req.decode);
 });
 
 app.listen(PORT, function () {
