d8f0281593227de4f1c8c8e7e556982478d7fcf5
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2023-06-24 07:51:40 -0400

    Hash password on signup post

--- controllers/signup-controller.js
+++ controllers/signup-controller.js
@@ -1,11 +1,17 @@
 const knex = require("knex")(require("../knexfile"));
 const bcrypt = require("bcrypt");
+// password: bcrypt.hashSync(user.password, Number(process.env.SALT_ROUNDS)),
 
 const addUser = (req, res) => {
   const { name, email, password } = req.body;
 
+  const hashedPassword = bcrypt.hashSync(
+    password,
+    Number(process.env.SALT_ROUNDS)
+  );
+
   knex("user")
-    .insert({ name, email, password })
+    .insert({ name, email, password: hashedPassword })
     .then((createdUser) => {
       res.status(201).json(createdUser);
     })
